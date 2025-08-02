4733643ce35d1ab11e50bd99a36da2ac68045008
parizval <anmolgoyal@gmail.com>
2025-08-02 23:38:54 +0530

    added final version of sdk with etherlink and tezos

---
+++ dist/types/src/fusion-order/constants.d.ts
@@ -0,0 +1,19 @@
+import { Address } from '@1inch/limit-order-sdk';
+export declare const CHAIN_TO_WRAPPER: {
+    1: Address;
+    137: Address;
+    56: Address;
+    42161: Address;
+    43114: Address;
+    100: Address;
+    8453: Address;
+    10: Address;
+    250: Address;
+    324: Address;
+    59144: Address;
+    130: Address;
+    146: Address;
+    11155111: Address;
+    128123: Address;
+    101: Address;
+};
--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,18 @@ export const ZX = '0x'
 
 export enum NetworkEnum {
     ETHEREUM = 1,
+    POLYGON = 137,
+    ZKSYNC = 324,
     BINANCE = 56,
     ARBITRUM = 42161,
+    AVALANCHE = 43114,
+    OPTIMISM = 10,
+    FANTOM = 250,
+    GNOSIS = 100,
+    COINBASE = 8453,
+    LINEA = 59144,
+    SONIC = 146,
+    UNICHAIN = 130,
     SEPOLIA = 11155111,
     ETHERLINK_TESTNET = 128123,
     TEZOS = 101
